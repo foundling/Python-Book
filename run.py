113951e7a0f3a3b24907f75d2895e3907f68aa7e
Alex Ramsdell <alexramsdell@gmail.com>
2016-02-04 18:08:44 -0800

    improve chapter routing and add toc

--- run.py
+++ run.py
@@ -4,7 +4,12 @@ from flask.ext.misaka import Misaka
 app = Flask(__name__)
 @app.route('/')
 def index():
-    return render_template('python_exercises.md')
+    return render_template('toc.html')
+
+@app.route('/ch/<id>')
+def chapter(id):
+    template = ''.join(['chapter_', id, '.md']);  
+    return render_template(template)
 
 Misaka(app)
 app.run(debug=True)
